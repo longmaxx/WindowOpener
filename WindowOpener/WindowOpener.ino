b8f2de1ff30e48b564848eceaace0ab065cc60b2
longmaxx <longmaxx@yandex.ru>
2017-04-25 09:51:20 +0300

    use DallasTemperature.h lib

--- WindowOpener/WindowOpener.ino
+++ WindowOpener/WindowOpener.ino
@@ -1,5 +1,6 @@
 #include <DS1307.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
 #include <Servo.h>
 #include <avr/sleep.h>
 #include <avr/wdt.h>
@@ -55,10 +56,13 @@ Servo SRV1;
 #define LED_PIN 13
 
 #define ONE_WIRE1_PIN 12
-OneWire ds(ONE_WIRE1_PIN);
-byte scratchpad[12];
+OneWire OneWirePort(ONE_WIRE1_PIN);
+DallasTemperature DT(&OneWirePort);
+DeviceAddress addr_DS18b20;
 float lastCelsium;
-bool flag_OneWire_CRC8_ERROR;
+
+
+
 
 #define CELSIUM_LEVEL_OPEN  27 
 #define CELSIUM_LEVEL_CLOSE 22
@@ -102,6 +106,8 @@ void setup()
     //flag_GoSleep = false;
   }
   init_ServoInitMoves();
+  // OneWire Initialization
+  init_Thermometer();
 }
 
 void loop(){
@@ -113,6 +119,7 @@ void loop(){
       timerLoop();
     }
     mainLoop();
+    batteryHealthLoop();
     setup_Timer2();
     setLED(LOW);
   }
@@ -135,16 +142,20 @@ void mainLoop(){
     }
 }
 
-void timerLoop(){
+void batteryHealthLoop()
+{
+  //TODO: battery charge control  
+}
+
+void timerLoop()
+{
   //refresh temperature
   Serial.println(F(">timerLoop begin"));
-  readDS18B20Scratchpad();
-  lastCelsium = getTemperatureCelsium();
+  DT.requestTemperatures();
+  lastCelsium = DT.getTempC(*addr_DS18b20);
   //move window 
   Serial.print(F("Temperature:"));
   Serial.print((int)lastCelsium);
-  Serial.print(" CRC_OK:");
-  Serial.println(!flag_OneWire_CRC8_ERROR); 
   //Serial.println("; Limits: ");// + String(CELSIUM_LEVEL_CLOSE) + "/" + String(CELSIUM_LEVEL_OPEN));
   signed char valBySensor = getNeededWindowStateBySensors();
   Serial.print("New servo position: ");
@@ -152,7 +163,6 @@ void timerLoop(){
   if (valBySensor != -1){
     moveServo1ToValue((unsigned char)valBySensor);
   }  
-  
   Serial.println(">timerLoop End");
 }
 
@@ -172,9 +182,6 @@ void moveServo1ToValue(byte value){
 }
 
 signed char getNeededWindowStateBySensors(){
-  if (flag_OneWire_CRC8_ERROR){
-    return -1;
-  }
   if (lastCelsium >= CELSIUM_LEVEL_OPEN){
     return SERVO1_OPENED_VAL;  
   }
@@ -290,91 +297,11 @@ void sleepNow(){         // here we put the arduino to sleep
 }
 
 //==================Thermometer================================
-void setTemperatureResolution(){
-    ds.reset();
-    ds.write(0xCC); // skip ROM
-    ds.write(0x4E);///write scratchpad
-    ds.write(0x00);//TH
-    ds.write(0x00);//TL
-    ds.write(0b01011111);//prefs
-}
-void readDS18B20Scratchpad(){
-  byte i;
-  ds.reset();
-  ds.write(0xCC);
-  //ds.reset();
-  ds.write(0x44); // start conversion
-  delay(400);     // wait conversion
-  // we might do a ds.depower() here, but the reset will take care of it.
-   
-  ds.reset();
-  ds.write(0xCC);//skip rom
-  //ds.reset();    
-  ds.write(0xBE);         // Read Scratchpad
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    scratchpad[i] = ds.read();
-  }
-  flag_OneWire_CRC8_ERROR = !((Make_CRC8(scratchpad,8) == (unsigned char)scratchpad[8]) && (scratchpad[8] != 0));
-}
-
-
-float getTemperatureCelsium(){
-  byte type_s = false;
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (scratchpad[1] << 8) | scratchpad[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (scratchpad[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - scratchpad[6];
-    }
-  } else {
-    byte cfg = (scratchpad[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  return (float)raw / 16.0;
-  
-}
-//==================End Thermometer============================
-
-//================= CRC Calculation==========================================
-const unsigned char CRC8Table[256] = {
-0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
-157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
-35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
-190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
-70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
-219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
-101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
-248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
-140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
-17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
-175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
-50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
-202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
-87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
-233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
-116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53};
-
-/*
-This procedure calculates the cumulative Dallas Semiconductor 1–Wire CRC of all bytes passed to it.
-*/
-unsigned char  Do_CRC8(unsigned char CRC, unsigned char X){
-  return CRC8Table[CRC ^ X];//xor
-}
-
-unsigned char Make_CRC8(unsigned char* arr, unsigned char len){
-  unsigned char tmpCRC8 = 0x00;
-  for (unsigned char i=0;i<len;i++){
-    tmpCRC8 = Do_CRC8(tmpCRC8,arr[i]);
-  }
-  return tmpCRC8;
+void init_Thermometer()
+{
+  DT.begin();
+  DT.getAddress(*addr_DS18b20,0);
+  DT.setResolution(*addr_DS18b20,TEMP_9_BIT);
+  DT.setWaitForConversion(true);// don't return from 'requestTemperatures()'  before conversion completed
 }
 
